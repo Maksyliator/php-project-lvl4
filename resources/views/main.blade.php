97c90179187ebae148299f05675e1700f9312a54
maksyliator <maksim.tankov@gmail.com>
2022-03-19 20:21:01 +0300

    Add authentication

---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="container py-4">
+        <div class="p-5 mb-4 bg-light border rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">{{ __('Task manager') }}</h1>
+            </div>
+        </div>
+    </main
+@endsection

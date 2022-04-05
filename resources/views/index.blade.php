8797dfa76ece536d9845fd74ae04a63b68f4b216
maksyliator <maksim.tankov@gmail.com>
2022-04-05 11:07:50 +0300

    Add string in index.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <main class="container-fluid py-5">
         <div class="p-5 mb-4 bg-light border rounded-3">
-            <div class="container">
-                <div class="jumbotron">
-                    <h1 class="display-3 fw-bold">{{ __('layout.name') }}</h1>
-                </div>
+            <div class="jumbotron">
+                <h1 class="display-4"> {{__('index.Hello_from_Hexlet!')}} </h1>
+                <p class="lead"> {{__('index.Practical_programming_courses')}} </p>
+                <hr class="my-4">
+                <a class="btn btn-primary btn-lg" href="https://hexlet.io" role="button"> {{__('index.Learn_more')}}</a>
             </div>
         </div>
     </main
